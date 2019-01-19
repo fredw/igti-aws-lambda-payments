f771e57dfcf5693817b27c61f3b1ec2173a10306
Frederico Wuerges Becker <fred.wuerges@gmail.com>
2019-01-19 20:18:31 +0100

    Removed unused commands on deploy.sh

--- assets/scripts/deploy.sh
+++ assets/scripts/deploy.sh
@@ -10,6 +10,3 @@ eval $(aws ecr get-login)
 # Build and upload the function on AWS
 make -C $TRAVIS_BUILD_DIR build
 make -C $TRAVIS_BUILD_DIR update
-
-# Update the handler name to the right name
-#aws lambda update-function-configuration --function-name=payments --handler=main
